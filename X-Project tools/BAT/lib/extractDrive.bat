0121f27241d64c086977d9b41ad7513e78d5d89c
ignacio-serrano <asdftglkjyh@yahoo.es>
2017-03-06 17:04:44 +0100

    Periodic backup of files.

--- X-Project tools/BAT/lib/extractDrive.bat
+++ X-Project tools/BAT/lib/extractDrive.bat
@@ -1,19 +1,26 @@
-::<?xml version="1.0" encoding="cp850"?>
-:: TODO: Probar
-:: SUBRUTINA extractDrive
-::   Devuelve la letra de unidad (y los dos puntos) del fichero pasado como par metro.
-:: USO: 
-::   CALL %lib%\extractDrive ®["]fileName["]¯ ®retVar¯
-:: Donde...
-::   ®["]fileName["]¯: Ruta absoluta o relativa del fichero. El entrecomillado 
-::                     es opcional si la ruta no contiene espacios y 
-::                     obligatorio en caso contrario.
-::   ®retVar¯:         Nombre de una variable existente o no a trav‚s de la que se 
-::                     devolver  la letra de unidad.
-
+::<?xml version="1.0" encoding="Cp850"?><contenido><![CDATA[
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: BEGINNING: SUBROUTINE ®extractDrive¯
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::    Obtains the drive letter (and the colon) of the file passed as parameter.
+::
+:: USAGE: 
+::    CALL :extractDrive ®["]fileName["]¯ ®retVar¯
+:: WHERE...
+::    ®["]fileName["]¯: Absolute or relative path of the file. If the path 
+::                      contains white spaces, it must be enclosed in double 
+::                      quotes. It is optional otherwise.
+::    ®retVar¯:         Name of a variable (existent or not) by means of which 
+::                      the drive letter will be returned.
+::
+:: DEPENDENCIES: NONE
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:extractDrive
 SETLOCAL
 SET retVar=%2
 ENDLOCAL & SET %retVar%=%~d2
 EXIT /B 0
-
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: END: SUBROUTINE ®extractDrive¯
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::]]></contenido>
